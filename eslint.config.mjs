e54a8be9630bfc17da0e95c3d70d562db0077608
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-08-01 19:36:05 +0100

    Move prettier config to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,16 @@ export default [
           code: 140,
         },
       ],
-      "prettier/prettier": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 120,
+          tabWidth: 2,
+          singleQuote: false,
+          arrowParens: "always",
+          semi: true,
+        },
+      ],
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": [
         "warn",
