4d55ac7664d600b37311687e53d97840bfbe3721
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-08-01 20:00:14 +0100

    Add tsconfig.json

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,3 @@
-async function main(env: NodeJS.ProcessEnv) {
+export async function main(env: NodeJS.ProcessEnv) {
   console.log("Hello World", env);
 }
-
-main(process.env).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
