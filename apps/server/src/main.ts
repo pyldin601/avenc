e168c51025030d4593a38936b7f01d6fd58d2857
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-08-01 20:23:21 +0100

    Introduce AWS clients and Config

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,3 +1,10 @@
+import { Config } from "./config";
+import { MediaConvertClient, S3Client } from "./aws-clients";
+
 export async function main(env: NodeJS.ProcessEnv) {
-  console.log("Hello World", env);
+  const config = Config.fromEnv(env);
+  const s3Client = new S3Client(config.awsAccessKeyId, config.awsSecretAccessKey);
+  const mediaConvertClient = new MediaConvertClient(config.awsAccessKeyId, config.awsSecretAccessKey);
+
+  console.log("Hello World");
 }
