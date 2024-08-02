1df178a62092155dce0228f8d25b15384f87569a
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-08-02 17:46:52 +0100

    Implement base version of media encoder

--- apps/encoder/src/media-encoder.ts
+++ apps/encoder/src/media-encoder.ts
@@ -1,5 +1,12 @@
+import fs from "node:fs";
+import { mkdtemp, rm, stat } from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+import { Readable } from "node:stream";
+import { finished } from "node:stream/promises";
+
 enum EncodingFormat {
-  MP3,
+  MP3 = "MP3",
 }
 
 interface EncodingParams {
@@ -14,13 +21,44 @@ interface EncodingContext {
 export class MediaEncoder {
   constructor(private readonly pathToFfmpeg: string) {}
 
-  public async encode(srcFile: string, dstFile: string, params: EncodingParams, ctx: EncodingContext): Promise<void> {
-    // TODO Read source file from signed source url
-    // TODO Encode file with desired encoding params
-    // TODO Upload encoded file to the signed dest url
-    // TODO Cleanup temp files
-    // TODO Handle errors
-    //
-    // TODO Stream and store encoding progress to the redis subject
+  public async encode(
+    srcUrl: string,
+    destUrl: string,
+    // For hinting dummy decoders if they can't recognize the source file format without knowing file extension.
+    srcExt: string,
+    params: EncodingParams,
+    ctx: EncodingContext,
+  ): Promise<void> {
+    // Create temp dir for encoding job
+    const tmpDir = await mkdtemp(`${os.tmpdir()}${path.sep}`);
+
+    // Download source file
+    const sourceFile = path.join(tmpDir, `input.${srcExt}`);
+    const srcRes = await fetch(srcUrl);
+    if (!srcRes.body) {
+      throw new Error("Unable to download the source file");
+    }
+    const fileStream = fs.createWriteStream(sourceFile, { flags: "wx" });
+    await finished(Readable.fromWeb(srcRes.body).pipe(fileStream));
+
+    // Encode file
+    const destFile = path.join(tmpDir, `output`);
+    // TODO ffmpeg: Encode file with desired encoding params
+
+    // Upload results to dest url
+    const stats = await stat(destFile);
+    const fileSizeInBytes = stats.size;
+
+    const readStream = fs.createReadStream(destFile);
+    await fetch(destFile, {
+      method: "put",
+      headers: {
+        "Content-Length": `${fileSizeInBytes}`,
+      },
+      body: readStream,
+    });
+
+    // Cleanup created files and directories
+    await rm(tmpDir, { recursive: true, force: true });
   }
 }
