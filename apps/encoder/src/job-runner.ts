2e9ce71307f78ee88b48f3d765fc56326ed13e9e
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-08-02 16:27:23 +0100

    Improve job processing loop

--- apps/encoder/src/job-runner.ts
+++ apps/encoder/src/job-runner.ts
@@ -1,5 +1,8 @@
 import { createClient, RedisClientType, RedisModules, RedisFunctions, RedisScripts } from "@redis/client";
-import { z } from "zod";
+import z from "zod";
+import makeDebug from "debug";
+
+const debug = makeDebug("JobRunner");
 
 const JobSchema = z.object({
   id: z.string(),
@@ -16,6 +19,8 @@ export class JobRunner {
 
     await redisClient.connect();
 
+    debug("Connected to redis host");
+
     return new JobRunner(redisClient);
   }
 
@@ -23,33 +28,52 @@ export class JobRunner {
     this.isRunning = true;
   }
 
-  public async addTask(task: unknown): Promise<void> {
-    await this.redisClient.zAdd(ENCODING_JOBS_KEY, { score: Date.now(), value: JSON.stringify(task) });
+  public async addJob(task: unknown): Promise<void> {
+    const rawJob = JSON.stringify(task);
+
+    debug("Adding new encoding job", rawJob);
+    await this.redisClient.zAdd(ENCODING_JOBS_KEY, { score: performance.now(), value: rawJob });
     // TODO: Notify suspended runners
   }
 
-  public async consumeTasks(): Promise<void> {
+  public async consumeJobs(): Promise<void> {
+    debug("Starting to consume jobs");
+
     while (this.isRunning) {
-      const rawJobs = await this.redisClient.zRangeByScore(ENCODING_JOBS_KEY, 0, 0);
+      const rawJobs = await this.redisClient.zRange(ENCODING_JOBS_KEY, 0, -1);
       const totalJobs = rawJobs.length;
+      debug(`Found ${totalJobs} in the queue`);
 
       if (totalJobs === 0) {
+        debug(`Suspending`);
         // TODO: Wait for a new job
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         break;
       }
 
       for (let i = 0; i < totalJobs; i += 1) {
         const rawJob = rawJobs[i];
-        const job = JobSchema.parse(rawJob);
+        debug("Reading job", rawJob);
 
+        const parseResult = JobSchema.safeParse(JSON.parse(rawJob));
+        if (!parseResult.success) {
+          debug("Unable to read the job; skipping", rawJob, parseResult.error);
+          await this.redisClient.zRem(ENCODING_JOBS_KEY, rawJobs[i]);
+          continue;
+        }
+
+        const job = parseResult.data;
         const success = await this.redisClient.hSetNX(CURRENT_ENCODING_JOBS_KEY, job.id, job.id);
         if (!success) {
+          debug("This job already consuming");
           // This task is already processing: skip
           continue;
         }
 
         // TODO: Process encoding job
+        debug("Processing job");
 
+        debug("Removing job from the queue");
         await this.redisClient.zRem(ENCODING_JOBS_KEY, rawJobs[i]);
         await this.redisClient.hDel(CURRENT_ENCODING_JOBS_KEY, job.id);
       }
--- apps/encoder/src/main.ts
+++ apps/encoder/src/main.ts
@@ -1,14 +1,19 @@
 import { JobRunner } from "./job-runner";
 import { Config } from "./config";
 import { MediaEncoder } from "./media-encoder";
+import { v4 } from "uuid";
 
 export async function main(env: NodeJS.ProcessEnv) {
   const config = Config.fromEnv(env);
   const mediaEncoder = new MediaEncoder(config.pathToFfmpeg);
   const jobRunner = await JobRunner.create(config.redisHost);
 
+  for (let i = 0; i <= 100; i += 1) {
+    await jobRunner.addJob({ id: v4() });
+  }
+
   try {
-    await Promise.all([jobRunner.consumeTasks()]);
+    await Promise.all([jobRunner.consumeJobs()]);
   } finally {
     await jobRunner.close();
   }
