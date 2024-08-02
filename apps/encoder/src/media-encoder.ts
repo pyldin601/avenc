d861dcf07bc862ddacc5b31f7b8e5908d9e8d6c4
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-08-02 17:13:28 +0100

    Add more todos to media encoder

--- apps/encoder/src/media-encoder.ts
+++ apps/encoder/src/media-encoder.ts
@@ -3,8 +3,20 @@ interface EncodingParams {
   bitrate: number | null;
 }
 
+interface EncodingContext {
+  encodingJobId: string;
+}
+
 export class MediaEncoder {
   constructor(private readonly pathToFfmpeg: string) {}
 
-  public async encode(srcFile: unknown, dstFile: unknown, params: EncodingParams): Promise<void> {}
+  public async encode(srcFile: unknown, dstFile: unknown, params: EncodingParams, ctx: EncodingContext): Promise<void> {
+    // TODO Read source file from signed source url
+    // TODO Encode file with desired encoding params
+    // TODO Upload encoded file to the signed dest url
+    // TODO Cleanup temp files
+    // TODO Handle errors
+    //
+    // TODO Stream and store encoding progress to the redis subject
+  }
 }
